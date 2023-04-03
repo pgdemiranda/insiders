8ccc45f89133903d152e1a6ba8c077f01a3011fe
Pablo Miranda <pgdemiranda@gmail.com>
2023-04-03 20:57:57 -0300

    papermill done, ready for deploy

--- run_model.sh
+++ run_model.sh
@@ -1,6 +1,8 @@
 # variable
-data=$(date +'%Y-%m-%dT%H:%M:%S')
-path='c:/Users/pgdem/repos/insiders'
-path_to_envs='c:/Users/pgdem/anaconda3/envs/insiders/Scripts'
+data=$(date +'%Y-%m-%dT%H-%M-%S')
 
-$path_to_envs/papermill $pathsrc/models/c10.0-pgdm-deploy.ipynb $path/reports/c10-mdfl-deploy_$data.ipynb
+# paths
+set path=C:\Users\pgdem\repos\insiders
+set path_to_envs=C:\Users\pgdem\anaconda3\envs\insiders\Scripts
+
+$path_to_envs\papermill $path\src/models/c10.0-pgdm-deploy.ipynb $path\reports/c10.0-pgdm-deploy_$data.ipynb && echo "Done!" || echo "Papermill not executed"
